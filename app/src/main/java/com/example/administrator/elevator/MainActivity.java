e667faa93fb34fa8f40d97899fb056abce04b6bc
linqingshanlinqingshan <1241607720@qq.com>
2019-02-14 16:57:09 +0800

    电梯模型，进度提示

--- app/src/main/java/com/example/administrator/elevator/MainActivity.java
+++ app/src/main/java/com/example/administrator/elevator/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.administrator.elevator;
 
 
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,12 +11,14 @@ import android.view.View;
 import android.view.ViewTreeObserver;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
 
     WebView web_view_left;
     WebView web_view_right;
@@ -27,15 +31,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     FrameLayout flt_container;
 
-    //    String url_bending_elevator = "file:///android_asset/bending_elevator/index.html";  //转角
-    String url_bending_elevator = "http://news.china.com.cn/txt/2019-02/13/content_74461985.htm";  //转角
-    //    String straight_elevator = "file:///android_asset/straight_elevator/index.html";  //直角
-    String straight_elevator = "http://www.xinhuanet.com/politics/2019-02/12/c_1124103849.htm";    //直角
+    String url_bending_elevator = "file:///android_asset/bending_elevator/index.html";  //转角
+    String straight_elevator = "file:///android_asset/straight_elevator/index.html";    //直角
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         flt_container = findViewById(R.id.flt_container);
 
@@ -76,6 +80,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 showWebView();
             }
         });
+
     }
 
 
@@ -83,6 +88,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     int height;
 
+    ProgressDialog leftDialog;
 
     private void showWebView() {
 
@@ -101,10 +107,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         webSettings_left.setAllowUniversalAccessFromFileURLs(true);
 
+
         web_view_left.loadUrl(url_bending_elevator);
+
+
+        leftDialog = new ProgressDialog(this);
+        leftDialog.setTitle("正在加载");
+        leftDialog.setMessage("请稍后...");
+        leftDialog.setIndeterminate(true);
+        leftDialog.setCancelable(false);
+
+
+        web_view_left.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+//                view.loadUrl(url);
+                return false;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                leftDialog.show();
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                leftDialog.dismiss();
+                leftDialog = null;
+            }
+        });
+
     }
 
     boolean isShow = false;
+
+    ProgressDialog rightDialog;
 
 
     @Override
@@ -112,55 +152,94 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()) {
             case R.id.tv_left:
 
-                tv_left.setTextColor(getResources().getColor(R.color.color_Black));
-                tv_right.setTextColor(getResources().getColor(R.color.color_Gray));
-
-//                web_view_left.setVisibility(View.VISIBLE);
-//                web_view_right.setVisibility(View.GONE);
-
-                llt_left.setVisibility(View.VISIBLE);
-                llt_right.setVisibility(View.GONE);
+                onLeft();
 
                 break;
             case R.id.tv_right:
 
-                tv_left.setTextColor(getResources().getColor(R.color.color_Gray));
-                tv_right.setTextColor(getResources().getColor(R.color.color_Black));
+                onRight();
+
+                break;
+        }
+    }
+
+
+    private void onLeft() {
+
+        tv_left.setTextColor(getResources().getColor(R.color.color_Black));
+        tv_right.setTextColor(getResources().getColor(R.color.color_Gray));
+
+//                web_view_left.setVisibility(View.VISIBLE);
+//                web_view_right.setVisibility(View.GONE);
+
+        llt_left.setVisibility(View.VISIBLE);
+        llt_right.setVisibility(View.GONE);
+    }
+
+
+    private void onRight() {
+
+        tv_left.setTextColor(getResources().getColor(R.color.color_Gray));
+        tv_right.setTextColor(getResources().getColor(R.color.color_Black));
 
 //                web_view_left.setVisibility(View.GONE);
 //                web_view_right.setVisibility(View.VISIBLE);
 
-                llt_left.setVisibility(View.GONE);
-                llt_right.setVisibility(View.VISIBLE);
+        llt_left.setVisibility(View.GONE);
+        llt_right.setVisibility(View.VISIBLE);
 
-                if (!isShow) {
-                    isShow = !isShow;
+        if (!isShow) {
+            isShow = !isShow;
 
-                    web_view_right = new WebView(getApplicationContext());
+            rightDialog = new ProgressDialog(this);
+            rightDialog.setTitle("正在加载");
+            rightDialog.setMessage("请稍后...");
+            rightDialog.setIndeterminate(true);
+            rightDialog.setCancelable(false);
+
+            web_view_right = new WebView(getApplicationContext());
 
 
-                    LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(width, height);
-                    web_view_right.setLayoutParams(layoutParams);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(width, height);
+            web_view_right.setLayoutParams(layoutParams);
 
-                    llt_right.addView(web_view_right);
+            llt_right.addView(web_view_right);
 
-                    WebSettings webSettings_right = web_view_right.getSettings();
-                    //设置WebView属性，能够执行Javascript脚本
-                    webSettings_right.setJavaScriptEnabled(true);
-                    //设置可以访问文件
-                    webSettings_right.setAllowFileAccess(true);
-                    //设置支持缩放
-                    webSettings_right.setBuiltInZoomControls(true);
+            WebSettings webSettings_right = web_view_right.getSettings();
+            //设置WebView属性，能够执行Javascript脚本
+            webSettings_right.setJavaScriptEnabled(true);
+            //设置可以访问文件
+            webSettings_right.setAllowFileAccess(true);
+            //设置支持缩放
+            webSettings_right.setBuiltInZoomControls(true);
 
-                    webSettings_right.setAllowUniversalAccessFromFileURLs(true);
+            webSettings_right.setAllowUniversalAccessFromFileURLs(true);
 
-                    web_view_right.loadUrl(straight_elevator);
+            web_view_right.loadUrl(straight_elevator);
 
+            web_view_right.setWebViewClient(new WebViewClient() {
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+//                    view.loadUrl(url);
+                    return false;
                 }
 
+                @Override
+                public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                    super.onPageStarted(view, url, favicon);
+                    rightDialog.show();
+                }
 
-                break;
+                @Override
+                public void onPageFinished(WebView view, String url) {
+                    super.onPageFinished(view, url);
+                    rightDialog.dismiss();
+                    rightDialog = null;
+                }
+            });
+
         }
+
     }
 
 
