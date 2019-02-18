ba0655c72b8df864ce58ab5266f198adccd6fafd
linqingshanlinqingshan <1241607720@qq.com>
2019-02-18 17:45:17 +0800

    电梯模型，新的平台

---
+++ app/src/main/java/com/example/administrator/elevator/BaseActivity.java
@@ -0,0 +1,8 @@
+package com.example.administrator.elevator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+public abstract class BaseActivity extends AppCompatActivity implements View.OnClickListener {
+}
